eaec61983d0abf1dcb1ac2d42240d695c9d82440
sbs1003 <sbs1003@gmail.com>
2025-04-06 12:49:10 +0900

    Add files via upload

---
+++ poet.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import getpass
+import os
+import streamlit as st
+import time
+
+load_dotenv(override=True) 
+
+print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
+print("GROQ_API_KEY:", os.getenv("GROQ_API_KEY"))
+
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+import streamlit as st
+
+st.title("This is a title")
+content = st.text_input("시의 주제를 제세해주세요!")
+
+if st.button("시 작성 요청하기"):
+    with st.spinner("시 작성 중"):
+        result = chat_model.predict(content + "에 대해 시를 써줘")
+        st.write(result)
+
+
+#st.write("시")
+# print(result)
+
+
+
+
